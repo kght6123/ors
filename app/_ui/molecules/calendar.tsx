e56e005f1126febe4b55e8bef2362ab7771c8997
kght6123 <i.feeling.song+github@gmail.com>
2023-07-25 23:52:22 +0900

    コンポーネントの追加

--- app/_ui/molecules/calendar.tsx
+++ app/_ui/molecules/calendar.tsx
@@ -15,22 +15,34 @@ const Base = ({ className, monthIndex, year }: Props) => {
     (_, i) => i + 1
   );
   return (
-    <div className={clsx("relative", className)}>
-      <div className="absolute">{year}年</div>
-      <div className="text-center">{monthIndex + 1}月</div>
-      <div className="grid grid-cols-7 place-content-center place-items-center [&>*]:h-14">
-        <div>日</div>
+    <div className={clsx("relative p-3", className)}>
+      <div className="absolute top-0 text-sm font-black text-gray-500">
+        {year}年
+      </div>
+      <div className="mb-6 text-center text-xl font-black">
+        {monthIndex + 1}月
+      </div>
+      <div className="grid grid-cols-7 place-content-center place-items-center text-sm font-black text-gray-700 [&>*]:h-14">
+        <div className="text-red-600">日</div>
         <div>月</div>
         <div>火</div>
         <div>水</div>
         <div>木</div>
         <div>金</div>
-        <div>土</div>
+        <div className="text-blue-600">土</div>
         {Array.from({ length: firstDay }, (_, i) => i + 1).map((day) => (
           <div key={`empty-${day}`}>&nbsp;</div>
         ))}
-        {daysArray.map((day) => (
-          <div key={day}>{day}</div>
+        {daysArray.map((day, index) => (
+          <div
+            className={clsx(
+              (firstDay + index + 1) % 7 === 0 && "text-blue-600",
+              (firstDay + index + 1) % 7 === 1 && "text-red-600"
+            )}
+            key={day}
+          >
+            {day}
+          </div>
         ))}
       </div>
     </div>
