0a8ee538fb10f2f180d83aaeaeedc053f0155976
kght6123 <i.feeling.song+github@gmail.com>
2023-07-27 00:26:22 +0900

    コンポーネントの追加

--- app/_ui/molecules/calendar.tsx
+++ app/_ui/molecules/calendar.tsx
@@ -4,24 +4,24 @@ import clsx from "clsx";
 
 export interface Props {
   className?: string;
-  monthIndex: number;
+  month: number;
   year: number;
+  day: number;
 }
 
-const Base = ({ className, monthIndex, year }: Props) => {
-  const firstDay = new Date(year, monthIndex, 1).getDay();
+const Base = ({ className, day, month, year }: Props) => {
+  const firstDay = new Date(year, month - 1, 1).getDay();
   const daysArray = Array.from(
-    { length: new Date(year, monthIndex + 1, 0).getDate() },
+    { length: new Date(year, month, 0).getDate() },
     (_, i) => i + 1
   );
+
   return (
     <div className={clsx("relative p-3", className)}>
       <div className="absolute top-0 text-sm font-black text-gray-500">
         {year}年
       </div>
-      <div className="mb-6 text-center text-xl font-black">
-        {monthIndex + 1}月
-      </div>
+      <div className="mb-6 text-center text-xl font-black">{month}月</div>
       <div className="grid grid-cols-7 place-content-center place-items-center text-sm font-black text-gray-700 [&>*]:h-14">
         <div className="text-red-600">日</div>
         <div>月</div>
@@ -33,15 +33,17 @@ const Base = ({ className, monthIndex, year }: Props) => {
         {Array.from({ length: firstDay }, (_, i) => i + 1).map((day) => (
           <div key={`empty-${day}`}>&nbsp;</div>
         ))}
-        {daysArray.map((day, index) => (
+        {daysArray.map((value, index) => (
           <div
             className={clsx(
               (firstDay + index + 1) % 7 === 0 && "text-blue-600",
-              (firstDay + index + 1) % 7 === 1 && "text-red-600"
+              (firstDay + index + 1) % 7 === 1 && "text-red-600",
+              value === day &&
+                "relative before:absolute before:h-9 before:w-9 before:rounded-full before:bg-indigo-600 before:text-white"
             )}
-            key={day}
+            key={value}
           >
-            {day}
+            {value}
           </div>
         ))}
       </div>
