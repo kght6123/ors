1ed321bd9fe1524390714a43b3dd9f6e36aa1645
kght6123 <i.feeling.song+github@gmail.com>
2023-08-04 01:38:30 +0900

    Changes to be committed: 	modified:   app/_ui/molecules/timeline.tsx

--- app/_ui/molecules/timeline.tsx
+++ app/_ui/molecules/timeline.tsx
@@ -9,8 +9,45 @@ export interface Props {
   className?: string;
 }
 
-const Base = ({ className }: Props) => {
-  return <div className={clsx("relative p-3", className)}></div>;
+const Base = ({ className, splitMinute = 60 }: Props) => {
+  const timelines = new Array((12 * 60) / splitMinute);
+  return (
+    <ul className={clsx("relative flex flex-col", className)}>
+      {timelines.map((_value, index) => (
+        <li
+          className="flex flex-row content-center items-center px-4 py-2 odd:bg-gray-50 even:bg-gray-100"
+          key={index}
+        >
+          <div className="w-16 pr-4 text-right text-sm font-black text-gray-500">
+            9:00
+          </div>
+          <div className="w-full">
+            <div className="flex h-12 content-center items-center justify-center rounded-2xl bg-error-500 text-sm font-bold text-white">
+              予約済み
+            </div>
+          </div>
+        </li>
+      ))}
+      <li className="flex flex-row content-center items-center px-4 py-2 odd:bg-gray-50 even:bg-gray-100">
+        <div className="w-16 pr-4 text-right text-sm font-black text-gray-500">
+          10:00
+        </div>
+        <div className="w-full">
+          <div className="h-12"></div>
+        </div>
+      </li>
+      <li className="flex flex-row content-center items-center px-4 py-2 odd:bg-gray-50 even:bg-gray-100">
+        <div className="w-16 pr-4 text-right text-sm font-black text-gray-500">
+          11:00
+        </div>
+        <div className="w-full">
+          <div className="flex h-12 content-center items-center justify-center rounded-2xl bg-error-500 text-sm font-bold text-white">
+            予約済み
+          </div>
+        </div>
+      </li>
+    </ul>
+  );
 };
 
 const Timeline = {
