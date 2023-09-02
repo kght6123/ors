55568305c9213ddbbba9e6ad54e5c10acc76e091
kght6123 <i.feeling.song+github@gmail.com>
2023-09-03 02:18:28 +0900

    コンポーネントの追加

--- app/(dataEntry)/reserve/[time]/details/register/route.ts
+++ app/(dataEntry)/reserve/[time]/details/register/route.ts
@@ -1,10 +1,30 @@
+import { db, reserveDateTimes, userDetails } from "#/schema";
 import { ReserveDetail } from "$/(dataEntry)/reserve/_schema";
+import { authOptions } from "$/api/auth/[...nextauth]/route";
 import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next";
 
 export async function POST(
   request: Request
   // { params }: { params: { time: string } }
 ) {
   const { realName, tel, time } = ReserveDetail.parse(await request.formData());
+  const session = await getServerSession(authOptions);
+  const userId = session?.user?.id;
+  if (!userId)
+    return NextResponse.json(
+      { error: "ログイン情報が存在しません。" },
+      { status: 401 }
+    );
+  db.insert(reserveDateTimes)
+    .values({
+      id: time.toString(),
+      reserved_at: new Date(time),
+      userId,
+    })
+    .onConflictDoUpdate({
+      set: { reserved_at: new Date(time) },
+      target: reserveDateTimes.userId,
+    });
   return NextResponse.json({ name: realName, tel, time });
 }
